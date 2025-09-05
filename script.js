ee6266b6a6271bf4f182289c768df6d4232748fc
Abbosi009 <abbosixan@gmail.com>
2025-09-05 17:18:25 +0500

    2

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+//1
 class Triangle {
     #leftside;
     #rightside;
@@ -21,4 +21,35 @@ const t = new Triangle(3, 4, 5);
 console.log('Triangle: ', t.getLeftSide(), t.getRightSide(), t.getWidth()   );
 t.setSides(7, 8, 9);
 console.log('Triangle after set: ', t.getLeftSide(), t.getRightSide(), t.getWidth()   );
+//2
+class EmployeeBase {
+    #id; #firstName; #lastName; #salary;
+    constructor(id, firstName, lastName, salary) {
+        this.#id = id;
+        this.#firstName = firstName;
+        this.#lastName = lastName;
+        this.#salary = salary;
+    }
+    getId() { return this.#id; }
+    getFirstName() { return this.#firstName; }
+    getLastName() { return this.#lastName; }
+    getSalary() { return this.#salary; }
+    setId(id) { this.#id = id; }
+    setFirstName(f) { this.#firstName = f; }
+    setLastName(l) { this.#lastName = l; }
+    setSalary(s) { this.#salary = s; }
+
+    getFullName() { return `${this.#firstName} ${this.#lastName}`; }
+    getAnnualSalary() { return this.#salary * 12; }
+    raiseSalary(percent) {
+        this.#salary = this.#salary + (this.#salary * percent / 100);
+        return this.#salary;
+    }
+}
+const emp = new EmployeeBase(1, 'Abbos', 'Programmer', 2000);
+console.log('Full name: ', emp.getFullName());
+console.log('Annual salary: ', emp.getAnnualSalary());
+console.log('Raised salary 25%: ', emp.raiseSalary(25));
+//3
+
 
